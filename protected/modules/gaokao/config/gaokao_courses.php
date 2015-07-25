71ce362f2a3ecbc1972ea0de2525884c8197ae1b
dajjwwx <dajjwwx@163.com>
2015-07-25 12:00:37 +0800

    gaokao/config/gaokao_courses.php更换course文件名为gaokao_course.php

---
+++ protected/modules/gaokao/config/gaokao_courses.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	array('id'=>1,'course'=>'语文','cname'=>'chinese'),
+	array('id'=>2,'course'=>'数学(文)','cname'=>'mathematics_wen'),
+	array('id'=>2,'course'=>'数学(理)','cname'=>'mathematics_li'),
+	array('id'=>3,'course'=>'英语','cname'=>'english'),
+	array('id'=>4,'course'=>'理综','cname'=>'lizong'),
+	array('id'=>5,'course'=>'文综','cname'=>'wenzong'),
+
+);
+?>
