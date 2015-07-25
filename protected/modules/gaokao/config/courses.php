cadc5d699846b706ad0452f23f97545dc80484b2
dajjwwx <dajjwwx@163.com>
2015-07-25 12:08:03 +0800

    修改课程设置

--- protected/modules/gaokao/config/courses.php
+++ protected/modules/gaokao/config/courses.php
@@ -5,8 +5,12 @@ return array(
 	array('id'=>3,'course'=>'英语','cname'=>'english'),
 	array('id'=>4,'course'=>'物理','cname'=>'physics'),
 	array('id'=>5,'course'=>'化学','cname'=>'chemistry'),
-	array('id'=>6,'course'=>'历史','cname'=>'history'),
-	array('id'=>7,'course'=>'政治','cname'=>'politics'),
-	array('id'=>8,'course'=>'地理','cname'=>'geography'),
+	array('id'=>6,'course'=>'生物','cname'=>'biology'),
+	array('id'=>7,'course'=>'历史','cname'=>'history'),
+	array('id'=>8,'course'=>'政治','cname'=>'politics'),
+	array('id'=>9,'course'=>'地理','cname'=>'geography'),
+	array('id'=>10,'course'=>'美术','cname'=>'art'),
+	array('id'=>11,'course'=>'音乐','cname'=>'music'),
+	array('id'=>12,'course'=>'通用技术','cname'=>'general_technology')
 );
 ?>
