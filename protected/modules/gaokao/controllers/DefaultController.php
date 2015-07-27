f5c96993729bc01826a177c638f87704d304cede
dajjwwx <dajjwwx@163.com>
2015-07-27 08:51:16 +0800

    测试新增省市县数据actionRegion()

--- protected/modules/gaokao/controllers/DefaultController.php
+++ protected/modules/gaokao/controllers/DefaultController.php
@@ -16,9 +16,31 @@ class DefaultController extends Controller
 		));
 	}
 
+	public function actionRegion()
+	{
+		$model = Area::model()->find(array(
+			'condition'=>'name = \'会东县\''
+		));
+
+		UtilHelper::dump($model->attributes);
+		UtilHelper::dump($model->city->attributes);
+
+		echo '------------------------------';
+
+		$city = City::model()->find(array(
+			'condition'=>'code = :code',
+			'params'=>array(
+				':code'=>$model->citycode
+			)
+		));
+
+		UtilHelper::dump($city->attributes);
+		UtilHelper::dump($city->province->attributes);
+	}
+
 	public function actionTest()
 	{
-		$link = Gaokao::model()->getPaperLink(9,1,2013);
+		$link = Gaokao::model()->getPaperLink(22,1,2013);
 
 		echo $link;
 	}
