17de47e7a6127c7737c183d19693b0e1e3615670
dajjwwx <dajjwwx@163.com>
2015-08-15 19:14:15 +0800

    修改province.php引用_list.php文件

--- protected/modules/gaokao/views/space/province.php
+++ protected/modules/gaokao/views/space/province.php
@@ -14,13 +14,13 @@ $this->menu=array(
 );
 ?>
 <blockquote>
-<ul class="list-unstyled" style="font-size:14px;line-height:1.5em;"><b>筛选条件</b>
-	<li>年份：
+<ul class="list-unstyled" style="font-size:14px;line-height:1.5em;"><h4>筛选条件</h4>
+	<li><span class="glyphicon glyphicon-minus" aria-hidden="true"></span> 年份：
 		<?php foreach($years as $year):?>
 			<?php echo CHtml::link($year.'年',array('space/province','id'=>$_GET['id'],'year'=>$year));?> / 
 		<?php endforeach;?>			
 	</li>
-	<li>省份：
+	<li><span class="glyphicon glyphicon-minus" aria-hidden="true"></span> 省份：
 		<?php foreach($provinces as $k=>$province):?>
 			<?php echo CHtml::link($province,array('space/province','id'=>$k,'year'=>$year));?> / 
 		<?php endforeach;?>
@@ -43,20 +43,22 @@ $this->menu=array(
 					'paper'=>'<span>试题</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>答案</span>'
 				)); ?>
 			<?php endforeach;?>
-			<?php else: ?>		
-			<?php				
-				$courses = Gaokao::model()->getCourses();
-				$chunks = array_chunk($courses, 6);
-			?>	
-			<?php foreach($chunks[0] as $course):?>
-				<?php $this->renderPartial('_list',array(
-					'id'=>$data->id,
-					'year'=>$current_year,
-					'name'=>$paper->name,
-					'course'=>$course['course'],
-					'paper'=>'<span>试题</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>答案</span>'
-				)); ?>
-			<?php endforeach;?>
+			<?php else: ?>	
+			<?php if($paper):?>	
+				<?php				
+					$courses = Gaokao::model()->getCourses();
+					$chunks = array_chunk($courses, 6);
+				?>	
+				<?php foreach($chunks[0] as $course):?>
+					<?php $this->renderPartial('_list',array(
+						'id'=>$data->id,
+						'year'=>$current_year,
+						'name'=>$paper->name,
+						'course'=>$course['course'],
+						'paper'=>'<span>试题</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>答案</span>'
+					)); ?>
+				<?php endforeach;?>
+			<?php endif;?>
 		<?php endif;?>	
 		</ul>		
 	</div>
