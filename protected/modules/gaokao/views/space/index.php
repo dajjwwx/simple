6fa96df9b64efd99d7a1afd1e53dee6e8bdb2905
dajjwwx <dajjwwx@163.com>
2015-08-15 19:14:53 +0800

    修改显示主页内容

--- protected/modules/gaokao/views/space/index.php
+++ protected/modules/gaokao/views/space/index.php
@@ -4,7 +4,8 @@
 
 $this->breadcrumbs=array(
 	$this->module->t('gaokao','Gaokao')=>array('space/index'),
-	$viewname
+	$viewname.'年高考真题',
+	$current_course
 );
 
 $this->menu=array(
@@ -12,16 +13,37 @@ $this->menu=array(
 	array('label'=>'Manage Gaokao', 'url'=>array('admin')),
 );
 ?>
+<blockquote>
+	<ul class="list-unstyled" style="font-size:14px;line-height:1.5em;"><h4>筛选条件</h4>
+		<li><span class="glyphicon glyphicon-minus" aria-hidden="true"></span> 年份：
+			<?php foreach($years as $year):?>
+				<?php echo CHtml::link($year.'年',array('space/index','id'=>$year,'course'=>$default_course));?> / 
+			<?php endforeach;?>			
+		</li>
+		<li><span class="glyphicon glyphicon-minus" aria-hidden="true"></span> 科目：
+			<?php foreach($courses as $course):?>
+				<?php echo CHtml::link($course['course'],array('space/index','id'=>$default_year,'course'=>$course['id']));?> / 
+			<?php endforeach;?>			
+		</li>	
+	</ul>		
+</blockquote>
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<span class="glyphicon glyphicon-paperclip"></span> <?php echo Yii::t('basic','Recent Updates');?></div>
+		<span class="glyphicon glyphicon-paperclip"></span> <?php echo $current_course;?></div>
 	<div class="panel-body">
-		<?php $this->widget('zii.widgets.CListView', array(
-			'dataProvider'=>$dataProvider,
-			'itemView'=>'_view',
-		)); ?>				
+		<ul style="margin:0px;padding:0px;">
+			<?php foreach($papernames as $data):?>
+				<?php $this->renderPartial('_list',array(
+					'id'=>$data->id,
+					'year'=>$data->year,
+					'name'=>$data->name,
+					'course'=>$current_course,
+					'paper'=>'<span>试题</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>答案</span>'
+				)); ?>
+
+
+		<?php endforeach;?>
+		</ul>				
 	</div>
 </div>
-
-
